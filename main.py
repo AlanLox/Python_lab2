497901bba6b65ea6707992abec3a378fbe91bb9f
AlanLox <alan_golub_lox@mail.ru>
2020-12-09 12:03:54 +0400

    Исправленный вариант (убраны циклы)

--- main.py
+++ main.py
@@ -7,29 +7,18 @@ import matplotlib.figure as figure
 from skimage import io
 
 def toGrayscaled(arr):
-    newArr = np.empty(shape=arr.shape)
-    for i in range(512):
-        for j in range(512):
-            list = arr[i, j]*[0.299, 0.587, 0.114]
-            tmp = list[0]+list[1]+list[2]
-            newArr[i, j] += [tmp, tmp, tmp]
-    newArr = newArr.astype(int)
-    img = Image.fromarray(newArr.astype(np.uint8))
+    arr = arr*[0.299, 0.587, 0.114]
+    arr = arr.sum(2)
+    img = Image.fromarray(arr.astype(np.uint8))
     img.save('Lena_grayscaled.png')
     return
 
 
 def toThresholded(arr):
-    newArr = np.empty(shape=arr.shape)
-    for i in range(512):
-        for j in range(512):
-            for k in range(3):
-                if arr[i, j, k] < 100:
-                    newArr[i, j, k] = 0
-                else:
-                    newArr[i, j, k] = arr[i, j, k]
-    newArr = newArr.astype(int)
-    img = Image.fromarray(newArr.astype(np.uint8))
+    mask = (arr >= 100).astype(int)
+    arr = arr*mask
+    arr = arr.astype(int)
+    img = Image.fromarray(arr.astype(np.uint8))
     img.save('Lena_thresholded.png')
     return
 
@@ -45,7 +34,7 @@ if re.match(r'^.+\.png$', path) and os.path.exists(path):
     print(arr.max(0).max(0))
     print('Средние значения: ')
     tmp = np.asarray([arr.size/3, arr.size/3, arr.size/3])
-    print((arr.sum(0).sum(0)//tmp).astype(int))
+    print(arr.mean(0).mean(0).astype(int))
 
     toGrayscaled(arr)
     arr = np.asarray(Image.open('Lena_grayscaled.png'))
@@ -53,8 +42,8 @@ if re.match(r'^.+\.png$', path) and os.path.exists(path):
 
     image = io.imread('Lena_thresholded.png')
     ax = plt.hist(image.ravel(), bins=256)
-    plt.xlabel('Интенсивность вроде')
-    plt.ylabel('Какие-то штуки')
+    plt.xlabel('Интенсивность')
+    plt.ylabel('Количество')
     plt.title('Гистограмма распределения яркости изображения')
     plt.show()
     Output: figure
